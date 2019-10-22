a504cc9541d0a4ba629d671eeb3d6b9e344730cb
Shane Honanie <shaneh425@gmail.com>
2019-10-22 14:24:09 -0700

    FrontEnd React Static Build

---
+++ PPMToolFullStack/src/main/resources/static/precache-manifest.0d8260327b467e47e2b8dcb608ed2201.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adf609384e87f6857e4a6e061d2f6ff5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6219b80bb1f398f110ba",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "0650edc28d059f79d12f",
+    "url": "/static/css/main.1e32b8fa.chunk.css"
+  },
+  {
+    "revision": "6219b80bb1f398f110ba",
+    "url": "/static/js/2.c69cc3dc.chunk.js"
+  },
+  {
+    "revision": "0650edc28d059f79d12f",
+    "url": "/static/js/main.55869316.chunk.js"
+  },
+  {
+    "revision": "48ed8f1f6f86543ce515",
+    "url": "/static/js/runtime-main.2e6edbb3.js"
+  }
+]);
